bdc6022dca86cc8b39119c6d6bd9ef2bde6af0a3
conan chan <15920025414@qq.com>
2021-02-01 15:35:02 +0800

    Revert "feat(all):java15"
    This reverts commit a8d22536b8d7b6957241853615d33fe68d6e66c3.

--- src/main/java/com/shenzhi/belina/mapper/UserMapper.java
+++
@@ -1,9 +0,0 @@
-package com.shenzhi.belina.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.shenzhi.belina.entity.User;
-
-
-public interface UserMapper extends BaseMapper<User> {
-
-}
